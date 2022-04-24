73eab73f00faccef6e95abb930fe6a2c7d747f44
Julien Maury <6878027+jmau111@users.noreply.github.com>
2022-04-25 06:31:16 +0900

    indent

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,7 +2,7 @@ const { createElement } = require(`react`)
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
   if (pluginOptions.domains !== false) {
-		const metas = pluginOptions.domains.map(domain => createElement(`link`, { rel: `dns-prefetch`, href: domain }))
-		setHeadComponents([metas])
+	  const metas = pluginOptions.domains.map(domain => createElement(`link`, { rel: `dns-prefetch`, href: domain }))
+	  setHeadComponents([metas])
   }
 }
