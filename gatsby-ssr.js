ef774ff8dc273925722599e7f6e634ba186d762c
Julien Maury <6878027+jmau111@users.noreply.github.com>
2022-05-02 12:04:30 +0200

    throw error when misconfigured

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,8 +1,13 @@
-const { createElement } = require(`react`)
+const { createElement } = require(`react`);
 
 exports.onRenderBody = ({ setHeadComponents }, pluginOptions) => {
-  if (pluginOptions.domains !== false) {
-	  const metas = pluginOptions.domains.map(domain => createElement(`link`, { rel: `dns-prefetch`, href: domain }))
-	  setHeadComponents([metas])
+  
+  if (pluginOptions.domains === undefined || pluginOptions.domains.length === 0) {
+    const error = 'You are misusing the plugin Gatsby DNS Prefetch, please provide domains in the plugin options.';
+    console.error(error);
+    throw new Error(error);
   }
+	
+  const metas = pluginOptions.domains.map(domain => createElement(`link`, { rel: `dns-prefetch`, href: domain }));
+  setHeadComponents([metas]);
 }
